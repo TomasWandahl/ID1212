0a708127a492e20ed20a59df6bcc36b3d30df007
TomasWandahl <Tomas.Wandahl@live.se>
2017-11-17 23:10:09 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package eu.tomaswandahl;
+
+import eu.tomaswandahl.Client.ClientView;
+
+public class Main {
+    public static void main(String[] args) {
+        new ClientView().start();
+    }
+}
